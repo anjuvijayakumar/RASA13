38e4aaddf9506bce1a23db8926a8f070fd18665d
anjuvijayakumar <36529100+anjuvijayakumar@users.noreply.github.com>
2019-09-17 13:54:40 +0530

    Update Generic.java
    edited by anju

--- src/qsp/Generic.java
+++ src/qsp/Generic.java
@@ -14,15 +14,14 @@ public class Generic {
 	{
 		System.setProperty("webdriver.gecko.driver",
 				"./softwares/geckodriver.exe");
-		 driver=new FirefoxDriver();
+		WebDriver driver=new FirefoxDriver();
 		driver.get("http://localhost/login.do;jsessionid=ojy6o54ugh8i");
 	}
 	
 	@AfterMethod
 	public void closeAppln() throws InterruptedException
 	{
-		Thread.sleep(3000);
-		driver.quit();
+		driver.close();
 	}
 }
 
